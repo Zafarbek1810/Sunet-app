60f7c12a25823c5a3b3227400f1fc5a54107ced5
Erkinovv <davlatyorwalker@gmail.com>
2023-02-23 14:55:27 +0500

    second page not full

---
+++ src/Components/Common/Svg/about1.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About1 = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.0"
+      width="401.000000pt"
+      height="539.000000pt"
+      viewBox="0 0 401.000000 539.000000"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <g
+        transform="translate(0.000000,539.000000) scale(0.100000,-0.100000)"
+        fill="#cdcdcd"
+        stroke="none"
+        style="&#10;"
+      ></g>
+    </svg>
+  );
+};
+
+export default About1;
--- src/Components/Pages/HomePage/SecondSection/SecondSection.style.js
+++ src/Components/Pages/HomePage/SecondSection/SecondSection.style.js
@@ -1,7 +1,83 @@
 import styled from "styled-components";
 
-const SecondSectionWrapper=styled.div`
-    min-height: 100vh;
-`
+const SecondSectionWrapper = styled.div`
+  min-height: 100vh;
+  background-color: #fff;
+  color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-export{SecondSectionWrapper}
+  section {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center; 
+    gap: 70px;
+  }
+  .left{
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+  }
+  .info-element{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 20px;
+  }
+  .info-element p{
+    padding-right: 30px;
+  }
+  .elemts{
+    display: flex;
+    flex-direction: column;
+  }
+  .right{
+    width: 50%;
+    text-align: center;
+  }
+  .left h4 {
+    margin: 0;
+    font-size: 14px;
+    font-weight: 800;
+    color: #7141b1;
+    position: relative;
+    display: inline-block;
+    margin-bottom: 7px;
+    text-transform: uppercase;
+    font-family: 'Nunito Sans';
+  }
+  .left h2{
+    font-size: 44px;
+    margin: 0;
+    line-height: 52px;
+    font-weight: 800;
+    font-family: 'Nunito Sans';
+  }
+  .left p{
+    font-size: 16px;
+    color: #6d6d6d;
+    font-weight: 400;
+    line-height: 30px;
+    font-family: 'Nunito Sans';
+  }
+  .elements img{
+    color: #7141B1;
+    width: 45px;
+    padding-bottom: 15px;
+  }
+  .elements h5{
+    margin: 0;
+    font-size: 18px; 
+    color: #000;
+    font-family: 'Nunito Sans';
+
+  }
+  .elements p{
+    font-size: 16px;
+    color: #6d6d6d;
+  }
+`;
+
+export { SecondSectionWrapper };
